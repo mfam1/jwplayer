ff2c041c7a5519854998520f98a0cf596aa6c0c8
mfam1 <71712432+mfam1@users.noreply.github.com>
2020-09-22 09:01:58 -0500

    Add files via upload

---
+++ di1.php
@@ -0,0 +1,15 @@
+<?php 
+include("core1.php");
+// Init
+$s = new YDrive();
+// get Resolution List
+//print_r($s->getResolution());
+// set new ID df
+$s->setID($_GET["id"] ?? "");
+// set Resolution
+$s->setResolution($_GET["m"]  ?? "");
+// download Link 
+$s->setDownload($_GET["alt"]  ?? "");
+// Start Stream
+$s->stream();
+?>
